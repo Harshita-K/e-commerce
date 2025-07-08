cf5a4d4a11806839f03512cb5651472a767406ee
harshita <harshita.kumari@students.iiit.ac.in>
2025-07-09 01:05:09 +0530

    git revert 17788d58fa6816aecc0239586db5a01ff6c86578

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,4 @@ app.get('/', (req, res) => {
     res.send("API working")
 })
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
